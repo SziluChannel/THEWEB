249633d598902e76daf12fd3946dcbb5053d1b80
SziluChannel <szaszilumin@gmail.com>
2023-02-10 17:49:00 +0100

    made lazy static database connection

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -5,23 +5,31 @@ use diesel::{pg::PgConnection, prelude::*, result::Error::DatabaseError, result:
 use models::{User, NewUser};
 use std::error::Error;
 use std::str::FromStr;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+lazy_static!{
+    static ref DATABASE_CONNECTION: Mutex<PgConnection> = Mutex::new({
+        let database_url = "postgres://appuser:appuser@localhost/theweb";
+        PgConnection::establish(database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to url: {}", database_url))
+    });
+}
 
 pub fn get_all_users() -> Vec<User>{
     use schema::users::dsl::*;
-    let connection = &mut establish_connection();
     let results = users
-        .load::<User>(connection)
+        .load::<User>(&mut *DATABASE_CONNECTION.lock().unwrap())
         .expect("Error loading users!!");
     results
 }
 
 pub fn get_user_by_email(mail: &str) -> Result<(String, String), String>{
     use schema::users::dsl::*;
-    let conn = &mut establish_connection();
     let res = users.filter(
         email.eq(mail))
         .select((id, password))
-        .get_result::<(uuid::Uuid, String)>(conn);
+        .get_result::<(uuid::Uuid, String)>(&mut *DATABASE_CONNECTION.lock().unwrap());
     match res {
         Ok((uid, hash)) => Ok((uid.to_string(), hash)),
         Err(e) => Err(e.to_string())
@@ -35,10 +43,9 @@ pub fn new_user(user: &NewUser) -> Result<String, String>{
         email: &user.email,
         password: &user.password,
     };
-    let conn = &mut establish_connection();
     let res = diesel::insert_into(users)
         .values(&new_user)
-        .get_result::<User>(conn);
+        .get_result::<User>(&mut *DATABASE_CONNECTION.lock().unwrap());
     match res {
         Ok(_) => Ok("Ok".to_string()),
         Err(e) => {
@@ -55,22 +62,15 @@ pub fn new_user(user: &NewUser) -> Result<String, String>{
 
 pub fn delete_user(user_id: &str) -> Result<(), Box<dyn Error>>{
     use schema::users::dsl::*;
-    let conn = &mut establish_connection();
-
     let user_id = uuid::Uuid::from_str(user_id)?;
     diesel::delete(
         users.filter(
             id.eq(user_id)
-        )).execute(conn)?;
+        )).execute(&mut *DATABASE_CONNECTION.lock().unwrap())?;
 
     Ok(())
 }
 
-fn establish_connection() -> PgConnection {
-    let database_url = "postgres://appuser:appuser@localhost/theweb";
-    PgConnection::establish(database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to url: {}", database_url))
-}
 
 
 
